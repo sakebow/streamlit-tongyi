ca63a93c6b8ffd3a5e393be04fb17438e850943d
sakebow <719878756@qq.com>
2025-08-05 17:08:27 +0800

    完善curl相关说明

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     mcp.run(transport="http", port=8000, path="/mcp")
     """
     启动服务后，请使用如下命令进行测试：
-    curl -N -X POST http://localhost:2024/mcp \
+    curl -N -X POST http://localhost:8000/mcp/ \
     -H "Content-Type: application/json" \
     -H "Accept: application/json, text/event-stream" \
     --data '{
@@ -41,10 +41,26 @@ if __name__ == "__main__":
         "id": 1,
         "method": "initialize",
         "params": {
-        "protocolVersion": "2025-03-26",
-        "capabilities": {},
-        "clientInfo": { "name": "curl", "version": "8.8" }
+            "protocolVersion": "2025-03-26",
+            "capabilities": {},
+            "clientInfo": { "name": "curl", "version": "8.8" }
         }
     }'
-
+    其中需要额外注意的是：
+    1. URL的末尾一定有斜线：/mcp/，如果没有，则会导致307
+    2. 请求头中的Accept字段必须是：application/json, text/event-stream
+        2.1. 不可以将他们单独设置，否则中间的分隔符号会变成`;`，导致406
+        2.2. 必须是两个同时有，否则会导致406
+    3. jsonrpc只有2.0这个选项
+    4. id自定义但不可为空
+    5. method是指要调用的RPC方法名
+        5.1. method不是指代码中的方法名，而是指RPC方法名
+        5.2. 此处参数只有固定的选项，initialize是其中之一
+    6. params是与方法绑定的参数对象
+        6.1. 同样的，不是指代码中的方法名，而是指RPC方法名
+        6.2. protocolVersion一般选择2025-03-26
+        6.3. capabilities表明期望启动的特性，同样只有固定的选项
+        6.4. clientInfo是客户端元数据
+    
+    完成上述命令之后，仅完成握手，即能力调研
     """
